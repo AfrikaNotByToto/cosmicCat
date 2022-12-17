f4f838fa63fd36c610a532ddb28a76e49fef5c5f
Tanya <alisaeva4498@mail.ru>
2022-12-18 02:44:16 +0300

    new class Cat in new file

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+const Cat = require('./Cat');
+
 /* eslint-disable no-restricted-globals */
 /* eslint-disable default-case */
 const canvas = document.getElementById('canvas');
@@ -8,86 +10,86 @@ const speed = 3; // скорость метеоритов
 const open = document.getElementById('modal-block');
 const close = document.getElementById('close');
 
-class Cat {
-  constructor(image, x, y, isPlayer) {
-    this.x = x;
-    this.y = y;
-    this.loaded = false;
-    this.dead = false;
-    this.isPlayer = isPlayer;
+// class Cat {
+//   constructor(image, x, y, isPlayer) {
+//     this.x = x;
+//     this.y = y;
+//     this.loaded = false;
+//     this.dead = false;
+//     this.isPlayer = isPlayer;
 
-    this.image = new Image();
+//     this.image = new Image();
 
-    const obj = this;
+//     const obj = this;
 
-    // eslint-disable-next-line prefer-arrow-callback
-    this.image.addEventListener('load', function () {
-      obj.loaded = true;
-    });
+//     // eslint-disable-next-line prefer-arrow-callback
+//     this.image.addEventListener('load', function () {
+//       obj.loaded = true;
+//     });
 
-    this.image.src = image;
-  }
+//     this.image.src = image;
+//   }
 
-  Update() {
-    if (!this.isPlayer) {
-      this.y += speed;
-    }
+//   Update() {
+//     if (!this.isPlayer) {
+//       this.y += speed;
+//     }
 
-    if (this.y > canvas.height + 50) {
-      this.dead = true;
-    }
-  }
-  // столкновение объектов
+//     if (this.y > canvas.height + 50) {
+//       this.dead = true;
+//     }
+//   }
+//   // столкновение объектов
 
-  Collide(cat) {
-    let hit = false;
+//   Collide(cat) {
+//     let hit = false;
 
-    if (
-      this.y < cat.y + cat.image.height * scale &&
-      this.y + this.image.height * scale > cat.y
-    ) {
-      // If there is collision by y
-      if (
-        this.x + this.image.width * scale > cat.x &&
-        this.x < cat.x + cat.image.width * scale
-      ) {
-        // If there is collision by x
-        hit = true;
-      }
-    }
-    return hit;
-  }
+//     if (
+//       this.y < cat.y + cat.image.height * scale &&
+//       this.y + this.image.height * scale > cat.y
+//     ) {
+//       // If there is collision by y
+//       if (
+//         this.x + this.image.width * scale > cat.x &&
+//         this.x < cat.x + cat.image.width * scale
+//       ) {
+//         // If there is collision by x
+//         hit = true;
+//       }
+//     }
+//     return hit;
+//   }
 
-  Move(v, d) {
-    if (v === 'x') {
-      // Движение x
-      // eslint-disable-next-line no-param-reassign
-      d *= 2;
+//   Move(v, d) {
+//     if (v === 'x') {
+//       // Движение x
+//       // eslint-disable-next-line no-param-reassign
+//       d *= 2;
 
-      this.x += d; // Смена позиции
+//       this.x += d; // Смена позиции
 
-      // откат изменений, если машина ушла за пределы экрана
-      if (this.x + this.image.width * scale > canvas.width) {
-        this.x -= d;
-      }
+//       // откат изменений, если машина ушла за пределы экрана
+//       if (this.x + this.image.width * scale > canvas.width) {
+//         this.x -= d;
+//       }
 
-      if (this.x < 0) {
-        this.x = 0;
-      }
-      // Движение y
-    } else {
-      this.y += d;
+//       if (this.x < 0) {
+//         this.x = 0;
+//       }
+//       // Движение y
+//     } else {
+//       this.y += d;
 
-      if (this.y + this.image.height * scale > canvas.height) {
-        this.y -= d;
-      }
+//       if (this.y + this.image.height * scale > canvas.height) {
+//         this.y -= d;
+//       }
 
-      if (this.y < 0) {
-        this.y = 0;
-      }
-    }
-  }
-}
+//       if (this.y < 0) {
+//         this.y = 0;
+//       }
+//     }
+//   }
+// }
 
 let timer = null;
 const UPDATE_TIME = 1000 / 60;
