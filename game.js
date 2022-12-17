bbdeb3b6117fbd4c88d03e483d2ce27305f929df
Tanya <alisaeva4498@mail.ru>
2022-12-17 18:44:06 +0300

    movements

--- game.js
+++ game.js
@@ -39,15 +39,45 @@ class Cat {
       this.y < cat.y + cat.image.height * scale &&
       this.y + this.image.height * scale > cat.y
     ) {
-      //If there is collision by y
+      // If there is collision by y
       if (
         this.x + this.image.width * scale > cat.x &&
         this.x < cat.x + cat.image.width * scale
       ) {
-        //If there is collision by x
+        // If there is collision by x
         hit = true;
       }
     }
     return hit;
+  }
+
+  Move(v, d) {
+    if (v === 'x') {
+      // Движение x
+      // eslint-disable-next-line no-param-reassign
+      d *= 2;
+
+      this.x += d; // Смена позиции
+
+      // откат изменений, если машина ушла за пределы экрана
+      if (this.x + this.image.width * scale > canvas.width) {
+        this.x -= d;
+      }
+
+      if (this.x < 0) {
+        this.x = 0;
+      }
+      // Движение y
+    } else {
+      this.y += d;
+
+      if (this.y + this.image.height * scale > canvas.height) {
+        this.y -= d;
+      }
+
+      if (this.y < 0) {
+        this.y = 0;
+      }
+    }
   }
 }
