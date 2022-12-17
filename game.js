9d517b5f7ecde3ea05145b56def94850c84da13a
Tanya <alisaeva4498@mail.ru>
2022-12-17 18:30:51 +0300

    single quotes and main class car

---
+++ game.js
@@ -0,0 +1,21 @@
+class Car {
+  constructor(image, x, y, isPlayer) {
+    this.x = x;
+    this.y = y;
+    this.loaded = false;
+    this.dead = false;
+    this.isPlayer = isPlayer;
+
+    this.image = new Image();
+
+    const obj = this;
+
+    // eslint-disable-next-line prefer-arrow-callback
+    this.image.addEventListener('load', function () {
+      obj.loaded = true;
+    });
+
+    this.image.src = image;
+  }
+  
+}
